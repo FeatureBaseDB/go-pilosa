bd10c7421cf5c0b8c6ac76c5ab7c24e1ee51bb60
Yuce Tekol <yucetekol@gmail.com>
2017-03-14 23:13:39 +0300

    Added QueryResponse

--- response.go
+++ response.go
@@ -2,10 +2,83 @@ package pilosa
 
 import "github.com/pilosa/gopilosa/internal"
 
-// BitmapResult represents bitmap data in the response
+// QueryResponse represents the response from a Pilosa query
+type QueryResponse struct {
+	Results      []*QueryResult
+	ErrorMessage string
+	IsSuccess    bool
+}
+
+func newQueryResponseFromInternal(response *internal.QueryResponse) (*QueryResponse, error) {
+	if response.Err != "" {
+		return &QueryResponse{
+			ErrorMessage: response.Err,
+			IsSuccess:    false,
+		}, nil
+	}
+	results := make([]*QueryResult, 0, len(response.Results))
+	for _, r := range response.Results {
+		result, err := newQueryResultFromInternal(r)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+	return &QueryResponse{
+		Results:   results,
+		IsSuccess: true,
+	}, nil
+}
+
+// GetResult returns the first result or nil
+func (qr *QueryResponse) GetResult() *QueryResult {
+	if qr.Results == nil || len(qr.Results) == 0 {
+		return nil
+	}
+	return qr.Results[0]
+}
+
+// QueryResult represent one of the results in the response
+type QueryResult struct {
+	BitmapResult *BitmapResult
+	CountItems   []*CountResultItem
+	Count        uint64
+}
+
+func newQueryResultFromInternal(result *internal.QueryResult) (*QueryResult, error) {
+	var bitmapResult *BitmapResult
+	var err error
+	if result.Bitmap != nil {
+		bitmapResult, err = newBitmapResultFromInternal(result.Bitmap)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &QueryResult{
+		BitmapResult: bitmapResult,
+		CountItems:   countItemsFromInternal(result.Pairs),
+		Count:        result.N,
+	}, nil
+}
+
+// CountResultItem represents a result from TopN call
+type CountResultItem struct {
+	ID    uint64
+	Count uint64
+}
+
+func countItemsFromInternal(items []*internal.Pair) []*CountResultItem {
+	result := make([]*CountResultItem, 0, len(items))
+	for _, v := range items {
+		result = append(result, &CountResultItem{ID: v.Key, Count: v.Count})
+	}
+	return result
+}
+
+// BitmapResult represents a result from Bitmap, Union, Intersect, Difference and Range PQL calls
 type BitmapResult struct {
-	attributes map[string]interface{}
-	bits       []uint64
+	Attributes map[string]interface{}
+	Bits       []uint64
 }
 
 func newBitmapResultFromInternal(bitmap *internal.Bitmap) (*BitmapResult, error) {
@@ -14,18 +87,8 @@ func newBitmapResultFromInternal(bitmap *internal.Bitmap) (*BitmapResult, error)
 		return nil, err
 	}
 	result := &BitmapResult{
-		attributes: attrs,
-		bits:       bitmap.Bits,
+		Attributes: attrs,
+		Bits:       bitmap.Bits,
 	}
 	return result, nil
-}
-
-// GetAttributes returns the attributes of this bitmap result
-func (b *BitmapResult) GetAttributes() map[string]interface{} {
-	return b.attributes
-}
-
-// GetBits returns the bits of this bitmap result
-func (b *BitmapResult) GetBits() []uint64 {
-	return b.bits
 }
--- response_test.go
+++ response_test.go
@@ -29,21 +29,101 @@ func TestNewBitmapResultFromInternal(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed with error: %s", err)
 	}
-	assertMapEquals(t, targetAttrs, result.GetAttributes())
-	if !reflect.DeepEqual(targetBits, result.GetBits()) {
+	assertMapEquals(t, targetAttrs, result.Attributes)
+	if !reflect.DeepEqual(targetBits, result.Bits) {
 		t.Fatalf("Not equal")
 	}
 }
 
-func TestNewBitmapResultFromInternalFailure(t *testing.T) {
+func TestNewQueryResponseFromInternal(t *testing.T) {
+	targetAttrs := map[string]interface{}{
+		"name":       "some string",
+		"age":        uint64(95),
+		"registered": true,
+		"height":     1.83,
+	}
+	targetBits := []uint64{5, 10}
+	targetCountItems := []*CountResultItem{
+		&CountResultItem{ID: 10, Count: 100},
+	}
+	attrs := []*internal.Attr{
+		&internal.Attr{Key: "name", StringValue: "some string", Type: 1},
+		&internal.Attr{Key: "age", UintValue: 95, Type: 2},
+		&internal.Attr{Key: "registered", BoolValue: true, Type: 3},
+		&internal.Attr{Key: "height", FloatValue: 1.83, Type: 4},
+	}
+	bitmap := &internal.Bitmap{
+		Attrs: attrs,
+		Bits:  []uint64{5, 10},
+	}
+	pairs := []*internal.Pair{
+		&internal.Pair{Key: 10, Count: 100},
+	}
+	response := &internal.QueryResponse{
+		Results: []*internal.QueryResult{
+			&internal.QueryResult{Bitmap: bitmap},
+			&internal.QueryResult{Pairs: pairs},
+		},
+		Err: "",
+	}
+	qr, err := newQueryResponseFromInternal(response)
+	if err != nil {
+		t.Fatalf("Failed with error: %s", err)
+	}
+	if qr.ErrorMessage != "" {
+		t.Fatalf("ErrorMessage should be empty")
+	}
+	if !qr.IsSuccess {
+		t.Fatalf("IsSuccess should be true")
+	}
+	if len(qr.Results) != 2 {
+		t.Fatalf("Number of results should be 2")
+	}
+	if qr.Results[0] != qr.GetResult() {
+		t.Fatalf("GetResult() should return the first result")
+	}
+	if !reflect.DeepEqual(targetAttrs, qr.Results[0].BitmapResult.Attributes) {
+		t.Fatalf("The bitmap result should contain the attributes")
+	}
+	if !reflect.DeepEqual(targetBits, qr.Results[0].BitmapResult.Bits) {
+		t.Fatalf("The bitmap result should contain the bits")
+	}
+	if !reflect.DeepEqual(targetCountItems, qr.Results[1].CountItems) {
+		t.Fatalf("The response should include count items")
+	}
+}
+
+func TestNewQueryResponseWithErrorFromInternal(t *testing.T) {
+	response := &internal.QueryResponse{
+		Err: "some error",
+	}
+	qr, err := newQueryResponseFromInternal(response)
+	if err != nil {
+		t.Fatalf("Failed with error: %s", err)
+	}
+	if qr.ErrorMessage != "some error" {
+		t.Fatalf("The response should include the error message")
+	}
+	if qr.IsSuccess {
+		t.Fatalf("IsSuccess should be false")
+	}
+	if qr.GetResult() != nil {
+		t.Fatalf("If there are no results, GetResult should return nil")
+	}
+}
+
+func TestNewQueryResponseFromInternalFailure(t *testing.T) {
 	attrs := []*internal.Attr{
 		&internal.Attr{Key: "name", StringValue: "some string", Type: 99},
 	}
 	bitmap := &internal.Bitmap{
 		Attrs: attrs,
 	}
-	result, err := newBitmapResultFromInternal(bitmap)
-	if result != nil && err == nil {
+	response := &internal.QueryResponse{
+		Results: []*internal.QueryResult{&internal.QueryResult{Bitmap: bitmap}},
+	}
+	qr, err := newQueryResponseFromInternal(response)
+	if qr != nil && err == nil {
 		t.Fatalf("Should have failed")
 	}
 }
